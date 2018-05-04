69730c53a210a64ffdadcec27a70272f519fbb9b
justin <justin@hellorocket.org>
2018-05-04 06:19:56 +0000

    Finished 18. No problems.

--- ex18.py
+++ ex18.py
@@ -1,2 +1,22 @@
-#this one is like your script with argv
-
+# this one is like your script with argv
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+    
+# ok, *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+    
+# this just takes 1 argument
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+    
+# this one takes no arguments
+def print_none():
+    print('I got nothing.')
+    
+    
+print_two('justin', 'barrera')
+print_two_again('justin', 'barrera')
+print_one('one more')
+print_none()
